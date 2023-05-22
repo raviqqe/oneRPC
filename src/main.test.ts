1f8e8f18e3eefad960b1e66651a2ea7a954295cf
Yota Toyama <raviqqe@gmail.com>
2023-05-22 15:18:52 +1000

    Fix

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,49 +1,55 @@
 import { describe, expect, it } from "vitest";
 import { z } from "zod";
-import { query } from "./main.js";
+import { mutate, query } from "./main.js";
 
-describe(query.name, () => {
-  it("handles a JSON object", async () => {
-    const value = { foo: 42 };
-
-    const response = await query(
-      z.any(),
-      z.any(),
-      (value: object) => value
-    )(
+for (const [procedure, buildRequest] of [
+  [
+    query,
+    (value: unknown) =>
       new Request(
         `https://foo.com?input=${encodeURIComponent(JSON.stringify(value))}`
-      )
-    );
+      ),
+  ] as const,
+  [
+    mutate,
+    (value: unknown) =>
+      new Request("https://foo.com", {
+        body: JSON.stringify(value),
+        method: "POST",
+      }),
+  ] as const,
+]) {
+  describe(procedure.name, () => {
+    it("handles a JSON object", async () => {
+      const value = { foo: 42 };
 
-    expect(await response.json()).toEqual(value);
+      const response = await procedure(
+        z.any(),
+        z.any(),
+        (value: object) => value
+      )(buildRequest(value));
+
+      expect(await response.json()).toEqual(value);
+    });
+
+    it("handles null", async () => {
+      const response = await procedure(
+        z.any(),
+        z.any(),
+        () => null
+      )(buildRequest({}));
+
+      expect(await response.json()).toBe(null);
+    });
+
+    it("handles undefined", async () => {
+      const response = await procedure(
+        z.any(),
+        z.any(),
+        () => undefined
+      )(buildRequest({}));
+
+      expect(response.body).toBe(null);
+    });
   });
-
-  it("handles null", async () => {
-    const response = await query(
-      z.any(),
-      z.any(),
-      () => null
-    )(
-      new Request(
-        `https://foo.com?input=${encodeURIComponent(JSON.stringify({}))}`
-      )
-    );
-
-    expect(await response.json()).toBe(null);
-  });
-
-  it("handles undefined", async () => {
-    const response = await query(
-      z.any(),
-      z.any(),
-      () => undefined
-    )(
-      new Request(
-        `https://foo.com?input=${encodeURIComponent(JSON.stringify({}))}`
-      )
-    );
-
-    expect(response.body).toBe(null);
-  });
-});
+}

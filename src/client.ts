26589ce5fa63e47dac5bafc0306d9d5f7a233c62
Yota Toyama <raviqqe@gmail.com>
2023-05-22 17:20:21 +1000

    Fix

--- src/client.ts
+++ src/client.ts
@@ -1,31 +1,30 @@
+import { parseLines } from "@raviqqe/hidash/json.js";
 import {
   type QueryRequestHandler,
   type QueryStreamRequestHandler,
   type MutateRequestHandler,
 } from "./main.js";
+import { toIterable, toStringStream } from "@raviqqe/hidash/stream.js";
 
 export const query = async <T extends QueryRequestHandler<unknown, unknown>>(
   path: string,
   input: T["_input"]
 ): Promise<T["_output"]> => {
-  const parameters = new URLSearchParams({
-    input: encodeURIComponent(JSON.stringify(input)),
-  }).toString();
-
-  return procedure(new Request(`${path}?${parameters}`));
+  return procedure(buildQueryRequest(path, input));
 };
 
-export const queryStream = async <
+export const queryStream = async function* <
   T extends QueryStreamRequestHandler<unknown, unknown>
->(
-  path: string,
-  input: T["_input"]
-): Promise<T["_output"]> => {
-  const parameters = new URLSearchParams({
-    input: encodeURIComponent(JSON.stringify(input)),
-  }).toString();
+>(path: string, input: T["_input"]): AsyncIterable<T["_output"]> {
+  const response = await fetch(buildQueryRequest(path, input));
 
-  return procedure(new Request(`${path}?${parameters}`));
+  if (!response.body) {
+    throw new Error("Empty stream body");
+  }
+
+  yield* parseLines(toIterable(toStringStream(response.body))) as AsyncIterable<
+    T["_output"]
+  >;
 };
 
 export const mutate = async <T extends MutateRequestHandler<unknown, unknown>>(
@@ -39,6 +38,14 @@ export const mutate = async <T extends MutateRequestHandler<unknown, unknown>>(
       headers: { "content-type": "application/json" },
     })
   );
+
+const buildQueryRequest = (path: string, input: unknown): Request => {
+  const parameters = new URLSearchParams({
+    input: encodeURIComponent(JSON.stringify(input)),
+  }).toString();
+
+  return new Request(`${path}?${parameters}`);
+};
 
 const procedure = async <T extends MutateRequestHandler<unknown, unknown>>(
   request: Request

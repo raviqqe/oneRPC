92a6e2099a22f00711299fa6c228fb1a716ff62c
Yota Toyama <raviqqe@gmail.com>
2023-05-22 15:01:48 +1000

    Fix

---
+++ src/main.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from "vitest";
+import { z } from "zod";
+import { query } from "./main.js";
+
+describe(query.name, () => {
+  it("handles a json", async () => {
+    const value = { foo: 42 };
+
+    const response = await query(
+      z.any(),
+      z.any(),
+      (value: object) => value
+    )(
+      new Request(
+        `https://foo.com?input=${encodeURIComponent(JSON.stringify(value))}`
+      )
+    );
+
+    expect(await response.json()).toEqual(value);
+  });
+});

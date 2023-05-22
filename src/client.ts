229844c462ca52dc59818911c1a7d63000a0871a
Yota Toyama <raviqqe@gmail.com>
2023-05-22 16:20:33 +1000

    Fix

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import { type QueryRequestHandler } from "./main.js";
+import { type QueryRequestHandler, type MutateRequestHandler } from "./main.js";
 
 export const query = async <T extends QueryRequestHandler<unknown, unknown>>(
   path: string,
@@ -7,7 +7,26 @@ export const query = async <T extends QueryRequestHandler<unknown, unknown>>(
   const parameters = new URLSearchParams({
     input: encodeURIComponent(JSON.stringify(input)),
   }).toString();
-  const response = await fetch(new Request(`${path}?${parameters}`));
+
+  return procedure(new Request(`${path}?${parameters}`));
+};
+
+export const mutate = async <T extends MutateRequestHandler<unknown, unknown>>(
+  path: string,
+  input: T["_input"]
+): Promise<T["_output"]> => {
+  return procedure(
+    new Request(path, {
+      body: JSON.stringify(input),
+      headers: { "content-type": "application/json" },
+    })
+  );
+};
+
+const procedure = async <T extends MutateRequestHandler<unknown, unknown>>(
+  request: Request
+): Promise<T["_output"]> => {
+  const response = await fetch(request);
 
   return (await response.json()) as T["_output"];
 };

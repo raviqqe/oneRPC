25a0afc33a54cd412845a100a815d5c5d3e26243
Yota Toyama <raviqqe@gmail.com>
2023-05-21 22:27:21 -0700

    Initialize package (#1)

---
+++ src/main.test.ts
@@ -0,0 +1,72 @@
+import { map, toArray } from "@raviqqe/hidash/promise.js";
+import { toIterable, toStringStream } from "@raviqqe/hidash/stream.js";
+import { describe, expect, it } from "vitest";
+import { z } from "zod";
+import { mutate, query } from "./main.js";
+
+for (const [procedure, buildRequest] of [
+  [
+    query,
+    (value: unknown) =>
+      new Request(
+        `https://foo.com?input=${encodeURIComponent(JSON.stringify(value))}`
+      ),
+  ] as const,
+  [
+    mutate,
+    (value: unknown) =>
+      new Request("https://foo.com", {
+        body: JSON.stringify(value),
+        method: "POST",
+      }),
+  ] as const,
+]) {
+  describe(procedure.name, () => {
+    it("handles a JSON object", async () => {
+      const value = { foo: 42 };
+
+      const response = await procedure(
+        z.object({ foo: z.number() }),
+        z.object({ foo: z.number() }),
+        (value: object) => value
+      )(buildRequest(value));
+
+      expect(await response.json()).toEqual(value);
+    });
+
+    it("handles null", async () => {
+      const response = await procedure(
+        z.unknown(),
+        z.null(),
+        () => null
+      )(buildRequest({}));
+
+      expect(await response.json()).toBe(null);
+    });
+
+    it("handles undefined", async () => {
+      const response = await procedure(
+        z.unknown(),
+        z.undefined(),
+        () => undefined
+      )(buildRequest({}));
+
+      expect(response.body).toBe(null);
+    });
+
+    it("handles async iterable", async () => {
+      const value = { foo: 42 };
+
+      const response = await procedure(z.any(), z.any(), async function* () {
+        yield value;
+        yield value;
+      })(buildRequest({}));
+
+      expect(
+        await toArray(
+          map(toIterable(toStringStream(response.body!)), JSON.parse)
+        )
+      ).toEqual([value, value]);
+    });
+  });
+}
---
+++ src/main.ts
@@ -0,0 +1,77 @@
+import { isAsyncIterable, map } from "@raviqqe/hidash/promise.js";
+import { toByteStream, toStream } from "@raviqqe/hidash/stream.js";
+import { type ZodType } from "zod";
+
+type RawHandler<T, S> = (input: T) => S | Promise<S>;
+
+type RequestHandler = (request: Request) => Promise<Response>;
+
+type Validator<T> = ZodType<T> | ((data: unknown) => T);
+
+type ResponseBody = AsyncIterable<unknown> | object | null | undefined;
+
+const inputParameterName = "input";
+
+export const query = <T, S extends ResponseBody>(
+  inputValidator: Validator<T>,
+  outputValidator: Validator<S>,
+  handle: RawHandler<T, S>
+): RequestHandler =>
+  procedure(
+    (request) => {
+      const input = new URL(request.url).searchParams.get(inputParameterName);
+
+      if (!input) {
+        throw new Error("Input parameter not defined");
+      }
+
+      return JSON.parse(input) as unknown;
+    },
+    inputValidator,
+    outputValidator,
+    handle
+  );
+
+export const mutate = <T, S extends ResponseBody>(
+  inputValidator: Validator<T>,
+  outputValidator: Validator<S>,
+  handle: RawHandler<T, S>
+): RequestHandler =>
+  procedure(
+    (request) => request.json(),
+    inputValidator,
+    outputValidator,
+    handle
+  );
+
+const procedure =
+  <T, S extends ResponseBody>(
+    getInput: (request: Request) => Promise<unknown> | unknown,
+    inputValidator: Validator<T>,
+    outputValidator: Validator<S>,
+    handle: RawHandler<T, S>
+  ): RequestHandler =>
+  async (request) => {
+    try {
+      const data = validate(
+        outputValidator,
+        await handle(validate(inputValidator, await getInput(request)))
+      );
+
+      if (data === undefined) {
+        return new Response(undefined);
+      } else if (isAsyncIterable(data)) {
+        return new Response(toByteStream(toStream(map(data, JSON.stringify))));
+      }
+
+      return new Response(JSON.stringify(data), {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        headers: { "content-type": "application/json" },
+      });
+    } catch (error) {
+      return new Response(undefined, { status: 500 });
+    }
+  };
+
+const validate = <T>(validator: Validator<T>, data: unknown): T =>
+  validator instanceof Function ? validator(data) : validator.parse(data);

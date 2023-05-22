3afe331ce07d1614d98a4e13dbbe666c48e38b53
Yota Toyama <raviqqe@gmail.com>
2023-05-22 13:38:50 +1000

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-export const rpc = <T, S>(
-  _handle: (input: T) => S
-): ((request: Request) => Response) => undefined as never;
+type RequestHandler = (request: Request) => Promise<Response>;
+
+type RawHandler<T, S> = (input: T) => S | Promise<S>;
+
+export const query =
+  <T, S>(handle: RawHandler<T, S>): RequestHandler =>
+  (request) =>
+    handleError(async () => {
+      return new Response(
+        JSON.stringify(
+          await handle(JSON.parse(new URL(request.url).searchParams))
+        ),
+        { status: 200 }
+      );
+    });
+
+export const mutate =
+  <T, S>(handle: RawHandler<T, S>): RequestHandler =>
+  (request) =>
+    handleError(async () => {
+      return new Response(
+        JSON.stringify(await handle(JSON.parse(await request.json()))),
+        { status: 200 }
+      );
+    });
+
+const handleError = async (
+  handle: () => Promise<Response>
+): Promise<Response> => {
+  try {
+    return handle();
+  } catch (error) {
+    return new Response(undefined, { status: 500 });
+  }
+};

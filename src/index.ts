d64f21b5cc879907b490d418928755b6fd8ccf99
Yota Toyama <raviqqe@gmail.com>
2023-05-22 14:16:44 +1000

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,63 @@
-type RequestHandler = (request: Request) => Promise<Response>;
+import { type ZodType } from "zod";
 
 type RawHandler<T, S> = (input: T) => S | Promise<S>;
 
+type RequestHandler = (request: Request) => Promise<Response>;
+
+type Validator<T> = ZodType<T> | ((data: unknown) => T);
+
+type ResponseBody = BodyInit | object | null | undefined;
+
 const inputParameterName = "input";
 
-export const query =
-  <T, S>(handle: RawHandler<T, S>): RequestHandler =>
+export const query = <T, S extends ResponseBody>(
+  inputValidator: Validator<T>,
+  outputValidator: Validator<S>,
+  handle: RawHandler<T, S>
+): RequestHandler =>
+  invoke(
+    (request) => {
+      const input = new URL(request.url).searchParams.get(inputParameterName);
+
+      if (!input) {
+        throw new Error("Input parameter not defined");
+      }
+
+      return JSON.parse(input);
+    },
+    inputValidator,
+    outputValidator,
+    handle
+  );
+
+export const mutate = <T, S extends ResponseBody>(
+  inputValidator: Validator<T>,
+  outputValidator: Validator<S>,
+  handle: RawHandler<T, S>
+): RequestHandler =>
+  invoke((request) => request.json(), inputValidator, outputValidator, handle);
+
+const invoke =
+  <T, S extends ResponseBody>(
+    getInput: (request: Request) => Promise<unknown>,
+    inputValidator: Validator<T>,
+    outputValidator: Validator<S>,
+    handle: RawHandler<T, S>
+  ): RequestHandler =>
   (request) =>
-    handleError(
-      async () =>
-        new Response(
-          JSON.stringify(
-            await handle(
-              JSON.parse(
-                new URL(request.url).searchParams.get(inputParameterName) ??
-                  JSON.stringify(null)
-              )
-            )
-          ),
-          { status: 200 }
+    handleError(async () =>
+      buildResponse(
+        validate(
+          outputValidator,
+          await handle(validate(inputValidator, await getInput(request)))
         )
+      )
     );
 
-export const mutate =
-  <T, S>(handle: RawHandler<T, S>): RequestHandler =>
-  (request) =>
-    handleError(
-      async () =>
-        new Response(JSON.stringify(await handle(await request.json())), {
-          status: 200,
-        })
-    );
+const buildResponse = (data: BodyInit | object | null | undefined): Response =>
+  data instanceof ReadableStream || data === undefined
+    ? new Response(data)
+    : new Response(JSON.stringify(data));
 
 const handleError = async (
   handle: () => Promise<Response>
@@ -41,3 +68,6 @@ const handleError = async (
     return new Response(undefined, { status: 500 });
   }
 };
+
+const validate = <T>(validator: Validator<T>, data: unknown): T =>
+  validator instanceof Function ? validator(data) : validator.parse(data);

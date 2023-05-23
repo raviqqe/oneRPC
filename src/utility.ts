4b16f1821c9301159941444c6dff28036cd4a18b
Yota Toyama <raviqqe@gmail.com>
2023-05-23 19:59:46 +1000

    Fix

--- src/utility.ts
+++ src/utility.ts
@@ -16,8 +16,7 @@ export const getJsonBody = async (
     return await response.json();
   } catch (_error) {
     // Even when clients pass `new Request("url", { body: undefined })`,
-    // bodies are defined as `ReadableStream` in responses after transferred to
-    // the other side...
+    // bodies are defined as `ReadableStream` after transferred to the other side...
     // TODO Does inspection of content length headers work in general?
     return undefined;
   }

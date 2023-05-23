671b278034bb83c263ad2f96fb8f759ca1110b2a
Yota Toyama <raviqqe@gmail.com>
2023-05-23 03:02:37 -0700

    Fix bug (#33)
    - Fix
    - Fix bug

--- src/utility.ts
+++ src/utility.ts
@@ -3,7 +3,21 @@ export interface ErrorBody {
 }
 
 export const inputParameterName = "input";
+
 export const jsonHeaders = {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   "content-type": "application/json",
+};
+
+export const getJsonBody = async (
+  response: Request | Response
+): Promise<unknown> => {
+  try {
+    return (await response.json()) as unknown;
+  } catch (_error) {
+    // Even when clients pass `new Request("url", { body: undefined })`,
+    // bodies are defined as `ReadableStream` after transferred to the other side...
+    // TODO Does inspection of content length headers work in general?
+    return undefined;
+  }
 };

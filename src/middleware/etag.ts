30646c54e6254a4fc46354d56a8598bcb12c07f8
Yota Toyama <raviqqe@gmail.com>
2023-05-29 19:03:15 +1000

    Fix

--- src/middleware/etag.ts
+++ src/middleware/etag.ts
@@ -1,18 +1,37 @@
 import { type MiddlewareFunction } from "./utility.js";
+import { toIterable, toStringStream } from "@raviqqe/hidash/stream";
+import { toArray } from "@raviqqe/hidash/promise";
 
 export const etag: MiddlewareFunction = async (request, handle, { stream }) => {
-  if (stream) {
-    return handle(request);
+  const response = await handle(request);
+
+  if (stream || !response.body) {
+    return response;
   }
 
   const tag = request.headers.get("if-none-match");
-  const response = await handle(request);
-  const newTag = await crypto.subtle.digest("sha1", response.body);
+  const newTag = decodeTag(
+    await crypto.subtle.digest("sha1", await collectStream(response.body))
+  );
 
   if (tag === newTag) {
-    return response;
+    return new Response(null, {
+      headers: response.headers,
+      status: 304,
+    });
   }
 
   response.headers.set("etag", newTag);
   return response;
 };
+
+const collectStream = async (
+  stream: ReadableStream<Uint8Array>
+): Promise<ArrayBuffer> => {
+  return new TextEncoder().encode(
+    (await toArray(toIterable(toStringStream(stream)))).join("")
+  );
+};
+
+const decodeTag = (buffer: ArrayBuffer): string =>
+  btoa(String.fromCharCode(...new Uint8Array(buffer)));

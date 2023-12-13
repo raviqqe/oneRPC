ef204b192f40f368e5ddc0a0eac75716e146961c
Yota Toyama <raviqqe@gmail.com>
2023-12-14 08:55:04 +0900

    Fix

--- src/adapter/aws-lambda.ts
+++ src/adapter/aws-lambda.ts
@@ -1,5 +1,17 @@
-import { Handler } from "aws-lambda";
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { RequestHandler } from "../main.js";
+import { toString, toStringStream } from "@raviqqe/loscore/async";
 
-export const awsLambda = (handler: RequestHandler): Handler => {
-  foo;
-};
+export const awsLambda =
+  (handler: RequestHandler): APIGatewayProxyHandlerV2 =>
+  async (event) => {
+    const response = await handler(event);
+
+    return {
+      statusCode: response.status,
+      headers: Object.fromEntries(response.headers.entries()),
+      body: response.body
+        ? await toString(toStringStream(response.body))
+        : undefined,
+    };
+  };

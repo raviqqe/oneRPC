bfed765c672efbfd0808d7ba96a0ea23cb158ffa
Yota Toyama <raviqqe@gmail.com>
2023-12-14 09:15:05 +0900

    Fix

--- src/adapter/aws-lambda.ts
+++ src/adapter/aws-lambda.ts
@@ -1,11 +1,18 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { RequestHandler } from "../main.js";
 import { toString, toStringStream } from "@raviqqe/loscore/async";
+import { isString, filterValues } from "@raviqqe/loscore";
 
 export const awsLambda =
   (handler: RequestHandler): APIGatewayProxyHandlerV2 =>
-  async (event) => {
-    const response = await handler(new Request({}));
+  async ({ body, headers, requestContext: { http } }) => {
+    const response = await handler(
+      new Request(new URL(http.path), {
+        method: http.method,
+        body,
+        headers: Object.fromEntries([...filterValues(headers, isString)]),
+      }),
+    );
 
     return {
       statusCode: response.status,

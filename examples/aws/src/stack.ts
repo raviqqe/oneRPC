9eb8930794f9ea8549a575b47126801320ad2eac
Yota Toyama <raviqqe@gmail.com>
2023-12-16 17:25:56 +0900

    Fix

--- examples/aws/src/stack.ts
+++ examples/aws/src/stack.ts
@@ -1,11 +1,38 @@
-import { Stack, type StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, type StackProps } from "aws-cdk-lib";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import {
+  AllowedMethods,
+  Distribution,
+  FunctionEventType,
+} from "aws-cdk-lib/aws-cloudfront";
 import { type Construct } from "constructs";
+import { HttpOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 export class MainStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new NodejsFunction(this, "square", { entry: "src/lambda/square.ts" });
+    const lambda = new NodejsFunction(this, "Function", {
+      entry: "src/lambda/square.ts",
+      runtime: Runtime.NODEJS_LATEST,
+    });
+
+    const distribution = new Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: new HttpOrigin("onerpc.raviqqe.com"),
+        allowedMethods: AllowedMethods.ALLOW_ALL,
+        functionAssociations: [
+          {
+            function: lambda,
+            eventType: FunctionEventType.VIEWER_REQUEST,
+          },
+        ],
+      },
+    });
+
+    new CfnOutput(this, "DistributionDomain", {
+      value: distribution.domainName,
+    });
   }
 }

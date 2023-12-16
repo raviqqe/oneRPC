d362297ff95e370adbf4ce2330856ffbcca1b391
Yota Toyama <raviqqe@gmail.com>
2023-12-16 18:21:46 +0900

    Fix

--- examples/aws/src/stack.ts
+++ examples/aws/src/stack.ts
@@ -1,5 +1,9 @@
 import { CfnOutput, Fn, Stack, type StackProps } from "aws-cdk-lib";
-import { AllowedMethods, Distribution } from "aws-cdk-lib/aws-cloudfront";
+import {
+  AllowedMethods,
+  Distribution,
+  OriginRequestPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
 import { HttpOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
@@ -19,7 +23,9 @@ export class MainStack extends Stack {
       defaultBehavior: {
         allowedMethods: AllowedMethods.ALLOW_ALL,
         origin: new HttpOrigin(Fn.parseDomainName(url.url)),
+        originRequestPolicy: OriginRequestPolicy.ALL_VIEWER,
       },
+      enableLogging: true,
     });
 
     new CfnOutput(this, "Domain", {

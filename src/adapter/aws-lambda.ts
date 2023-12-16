86ff9108f02136e982a9decbc422da3bc2c40a0d
Yota Toyama <raviqqe@gmail.com>
2023-12-16 18:45:47 +0900

    Fix

--- src/adapter/aws-lambda.ts
+++ src/adapter/aws-lambda.ts
@@ -1,28 +1,44 @@
-import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { CloudFrontRequestHandler, CloudFrontResultResponse } from "aws-lambda";
 import { RequestHandler } from "../main.js";
 import {
   collectString,
   toIterable,
   toStringStream,
 } from "@raviqqe/loscore/async";
-import { isString, filterValues } from "@raviqqe/loscore";
+import { map } from "@raviqqe/loscore";
 
 export const awsLambda =
-  (handler: RequestHandler): APIGatewayProxyHandlerV2 =>
-  async ({ body, headers, rawPath, requestContext: { http } }) => {
+  (handler: RequestHandler): CloudFrontRequestHandler =>
+  async ({ Records: [record] }) => {
+    if (!record) {
+      throw new Error("Invalid record in CloudFront event");
+    }
+
+    const { request } = record.cf;
+
+    const headers = new Headers();
+
+    for (const [key, values] of Object.entries(request.headers)) {
+      for (const value of values) {
+        headers.append(key, value.value);
+      }
+    }
+
     const response = await handler(
-      new Request(new URL(rawPath), {
-        method: http.method,
-        body,
-        headers: filterValues(headers, isString),
+      new Request(new URL(request.uri), {
+        method: request.method,
+        body: request.body?.data,
+        headers,
       }),
     );
 
     return {
-      statusCode: response.status,
-      headers: Object.fromEntries(response.headers.entries()),
+      status: response.status.toString(),
+      headers: Object.fromEntries(
+        map(response.headers.entries(), ([key, value]) => [key, [{ value }]]),
+      ),
       body: response.body
         ? await collectString(toIterable(toStringStream(response.body)))
         : undefined,
-    };
+    } satisfies CloudFrontResultResponse;
   };

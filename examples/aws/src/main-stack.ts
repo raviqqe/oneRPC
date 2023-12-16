2f9d8dddb133dea0c923308f22d4c5f6044a28ce
Yota Toyama <raviqqe@gmail.com>
2023-12-16 15:40:29 +0900

    Fix

---
+++ examples/aws/src/main-stack.ts
@@ -0,0 +1,8 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class MainStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+  }
+}

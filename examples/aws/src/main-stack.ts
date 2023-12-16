c86f5dd90af5ca6424b6269534b2d336cdb370a0
Yota Toyama <raviqqe@gmail.com>
2023-12-16 15:57:16 +0900

    Fix linting

--- examples/aws/src/main-stack.ts
+++ examples/aws/src/main-stack.ts
@@ -1,5 +1,5 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
+import { Stack, type StackProps } from "aws-cdk-lib";
+import { type Construct } from "constructs";
 
 export class MainStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {

de9c36ed75d30152af3f81dd54bcaff0c5bbe23e
Yota Toyama <raviqqe@gmail.com>
2023-12-16 16:41:15 +0900

    Fix linting

--- examples/aws/src/lambda/main.ts
+++ examples/aws/src/lambda/main.ts
@@ -1,6 +1,6 @@
-import { Handler } from "aws-lambda";
-import { awsLambda } from "onerpc/adapter/aws-lambda";
+import { type Handler } from "aws-lambda";
 import { query } from "onerpc";
+import { awsLambda } from "onerpc/adapter/aws-lambda";
 import { number } from "valibot";
 
 export const handler: Handler = awsLambda(

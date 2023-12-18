e3e46a9a47751989d467d525ff65dd904ebd06c2
Yota Toyama <raviqqe@gmail.com>
2023-12-19 00:05:46 +0900

    Fix

--- examples/next/src/app/page.tsx
+++ examples/next/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Client } from "onerpc/client.js";
 import { type GET } from "./api/foo/route.js";
 
 const client = new Client({
-  baseUrl: process.env.VERCEL_URL ?? "http://localhost",
+  baseUrl: process.env.VERCEL_URL ?? "http://localhost:3000",
 });
 
 export default async (): Promise<JSX.Element> => (

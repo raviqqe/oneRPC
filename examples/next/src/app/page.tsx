984b24b5bb5c2815b7ea20be1e5e049c4b1dc09a
Yota Toyama <raviqqe@gmail.com>
2023-12-18 23:48:16 +0900

    Fix

---
+++ examples/next/src/app/page.tsx
@@ -0,0 +1,6 @@
+import { type GET } from "./api/foo/route.js";
+import { query } from "onerpc/client.js";
+
+export default async (): Promise<JSX.Element> => (
+  <div>{await query<typeof GET>("/api/foo", 42))}</div>
+);

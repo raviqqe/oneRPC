5d703199c9ca8ccf135606dbc7b17302df6f8689
Yota Toyama <raviqqe@gmail.com>
2024-06-08 20:54:58 +0900

    Fix

--- src/validation.ts
+++ src/validation.ts
@@ -1,12 +1,10 @@
 import { type BaseSchema, parse, type BaseIssue } from "valibot";
 import { type ZodType } from "zod";
-import { Validator } from "./main.js";
+import { type Validator } from "./main.js";
 
 export const valibot =
   <T>(schema: BaseSchema<T, T, BaseIssue<unknown>>): Validator<T> =>
-  (data) => {
-    return parse(schema, data);
-  };
+  (data) => parse(schema, data);
 
 export const zod =
   <T>(type: ZodType<T>): Validator<T> =>

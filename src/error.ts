acebeab1f218904971e0e69054a148652cb51c71
Yota Toyama <raviqqe@gmail.com>
2025-08-11 08:48:02 -0700

    Fix

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,10 @@
 export class RpcError extends Error {
   public readonly status?: number;
 
-  public constructor(message?: string, options?: ErrorOptions & { status?: number }) {
+  public constructor(
+    message?: string,
+    options?: ErrorOptions & { status?: number },
+  ) {
     super(message, options);
 
     this.status = options?.status;

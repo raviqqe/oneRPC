016b74475ac29a3e6a39c82c653d718a9f17cbbe
Yota Toyama <raviqqe@gmail.com>
2025-09-14 16:55:57 -0700

    Fix

--- src/adapter/aws-lambda.ts
+++ src/adapter/aws-lambda.ts
@@ -1,4 +1,4 @@
-import { filterValues, isString } from "@raviqqe/loscore";
+import { filterValues, isString } from "es-toolkit";
 import {
   collectString,
   toIterable,
@@ -10,26 +10,26 @@ import type { RequestHandler } from "../index.js";
 
 export const awsLambda =
   (handler: RequestHandler): LambdaFunctionURLHandler =>
-  async (request) => {
-    const url = new URL("http://localhost");
+    async (request) => {
+      const url = new URL("http://localhost");
 
-    url.hostname = request.requestContext.domainName;
-    url.pathname = request.rawPath;
-    url.search = request.rawQueryString;
+      url.hostname = request.requestContext.domainName;
+      url.pathname = request.rawPath;
+      url.search = request.rawQueryString;
 
-    const response = await handler(
-      new Request(url, {
-        body: request.body,
-        headers: new Headers(filterValues(request.headers, isString)),
-        method: request.requestContext.http.method,
-      }),
-    );
+      const response = await handler(
+        new Request(url, {
+          body: request.body,
+          headers: new Headers(filterValues(request.headers, isString)),
+          method: request.requestContext.http.method,
+        }),
+      );
 
-    return {
-      body: response.body
-        ? await collectString(toIterable(toStringStream(response.body)))
-        : undefined,
-      headers: Object.fromEntries(response.headers.entries()),
-      statusCode: response.status,
+      return {
+        body: response.body
+          ? await collectString(toIterable(toStringStream(response.body)))
+          : undefined,
+        headers: Object.fromEntries(response.headers.entries()),
+        statusCode: response.status,
+      };
     };
-  };

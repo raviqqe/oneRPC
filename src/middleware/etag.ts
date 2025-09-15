e01d7444a694b8b8e789c1c2b9ca11ab285a2d80
Yota Toyama <raviqqe@gmail.com>
2025-09-14 17:40:05 -0700

    Fix

--- src/middleware/etag.ts
+++ src/middleware/etag.ts
@@ -3,33 +3,33 @@ import type { MiddlewareFunction } from "./utility.js";
 
 export const etag =
   ({ weak }: { weak?: boolean } = {}): MiddlewareFunction =>
-  async (request, handle, { stream }) => {
-    const response = await handle(request);
+    async (request, handle, { stream }) => {
+      const response = await handle(request);
 
-    if (stream || !response.body) {
-      return response;
-    }
+      if (stream || !response.body) {
+        return response;
+      }
 
-    const newResponse = response.clone();
-    const hash = encodeTag(
-      await crypto.subtle.digest("sha-1", await collectStream(response.body)),
-    );
-    const etag = `${weak ? "W/" : ""}"${hash}"`;
+      const newResponse = response.clone();
+      const hash = encodeTag(
+        await crypto.subtle.digest("sha-1", await collectStream(response.body)),
+      );
+      const etag = `${weak ? "W/" : ""}"${hash}"`;
 
-    if (etag === request.headers.get("if-none-match")) {
-      return new Response(null, {
-        headers: { etag },
-        status: 304,
-      });
-    }
+      if (etag === request.headers.get("if-none-match")) {
+        return new Response(null, {
+          headers: { etag },
+          status: 304,
+        });
+      }
 
-    newResponse.headers.set("etag", etag);
-    return newResponse;
-  };
+      newResponse.headers.set("etag", etag);
+      return newResponse;
+    };
 
 const collectStream = async (
-  stream: ReadableStream<Uint8Array<ArrayBufferLike>>,
-): Promise<Uint8Array<ArrayBufferLike>> =>
+  stream: ReadableStream<Uint8Array<ArrayBuffer>>,
+): Promise<Uint8Array<ArrayBuffer>> =>
   new TextEncoder().encode(
     (
       await Array.fromAsync(
